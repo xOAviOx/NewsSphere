c44782f84173abf023e13df4c7d20812370a2ee5
xOAviOx <avishuklacode@gmail.com>
2025-01-22 21:10:26 +0530

    Dates functinality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import { API_KEY } from "./api.js";
+const button = document.querySelector(".btn");
+const input = document.getElementById("search");
 const from = flatpickr("#datepickerFrom", {
   dateFormat: "Y-m-d", // Format like 2025-01-01
 });
@@ -6,15 +8,20 @@ const to = flatpickr("#datepickerTo", {
   dateFormat: "Y-m-d", // Format like 2025-01-01
 });
 
-const fromdate = from.input.value;
-const todate = to.input.value;
-console.log(fromdate, todate);
-const getData = async function (query) {
+button.addEventListener("click", function () {
+  let query = input.value;
+  let fromdate = from.input.value;
+  let todate = to.input.value;
+  console.log(fromdate, todate);
+  getData(query, fromdate, todate);
+  input.value = from.input.value = to.input.value = "";
+});
+
+const getData = async function (query, fromdate, todate) {
   const resp = await fetch(
-    `https://newsapi.org/v2/everything?q=${query}&from=2025-01-21&to=2025-01-21&sortBy=popularity&apiKey=${API_KEY}`
+    `https://newsapi.org/v2/everything?q=${query}&from=${fromdate}&to=${todate}&sortBy=popularity&apiKey=${API_KEY}`
   );
   const data = await resp.json();
   console.log(data);
   console.log(data.articles);
 };
-getData("trump");
